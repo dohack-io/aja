cff4c61e89378173268dbc4e8ce90909600c793d
Niklas Eicker <git@nikl.me>
2019-09-29 00:54:06 +0200

    new gardens and second team

--- bin/seeds/dev.js
+++ bin/seeds/dev.js
@@ -32,29 +32,52 @@ exports.seed = async function(knex) {
     .insert([
       {
         name: "Pokemon FTW"
+      }, 
+      {
+        name: "Gardeners of god"
       }
     ]);
-  await knex("gardens")
-    .insert([
-      {
-        team_id: teams[0],
-        longitude: 7.465897,
-        latitude: 51.497679
-      },
-      {
-        longitude: 7.522553,
-        latitude: 51.518319
-      }
-    ]);
-  await knex("teammates")
-    .insert([
-      {
-        team_id: teams[0],
-        user_id: users[0]
-      },
-      {
-        team_id: teams[0],
-        user_id: users[1]
-      }
-    ]);
+  await knex("gardens").insert([
+    {
+      team_id: teams[0],
+      longitude: 7.5236,
+      latitude: 51.5028,
+      size: 2
+    },
+    {
+      team_id: teams[1],
+      longitude: 7.5146,
+      latitude: 51.5093,
+      size: 3
+    },
+    {
+      longitude: 7.5163,
+      latitude: 51.5012,
+      size: 1
+    },
+    {
+      longitude: 7.5261,
+      latitude: 51.5087,
+      size: 4
+    },
+    {
+      longitude: 7.532,
+      latitude: 51.5016,
+      size: 2
+    }
+  ]);
+  await knex("teammates").insert([
+    {
+      team_id: teams[0],
+      user_id: users[0]
+    },
+    {
+      team_id: teams[0],
+      user_id: users[1]
+    },
+    {
+      team_id: teams[1],
+      user_id: users[0]
+    }
+  ]);
 };
