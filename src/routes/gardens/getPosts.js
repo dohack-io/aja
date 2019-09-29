3e281e42eb1e10ece374b3ad1f26d4f3d89aa001
Niklas Eicker <git@nikl.me>
2019-09-29 09:53:38 +0200

    populate the author

--- src/routes/gardens/getPosts.js
+++ src/routes/gardens/getPosts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const logger = require("../../logger");
 const httpContext = require("express-http-context");
 const Post = require('../../models/post');
+const User = require('../../models/user');
 
 router.get("/garden/:id/posts", async (req, res, next) => {
   logger.info("GET /garden/%s/posts", req.params.id, {
@@ -12,7 +13,15 @@ router.get("/garden/:id/posts", async (req, res, next) => {
     next(new ValidationError("Invalid garden Id"));
     return;
   }
-  const posts = await Post.getByGardenId(req.params.id);
+  let posts = await Post.getByGardenId(req.params.id);
+  const authors = await Promise.all(posts.map(post => post.author).filter((v, i, a) => a.indexOf(v) === i).map(author => User.getById(author).then(author => {
+    delete author.password;
+    return author;
+  })))
+  posts = posts.map(post => {
+    post.author = authors.find(author => post.author === author.id);
+    return post;
+  })
   res.result = posts;
   next();
 });
